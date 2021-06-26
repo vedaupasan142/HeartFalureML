693aa8dc571a113d1c53f68b55490be28e36c1f4
VEDA UPASAN P <60031358+vedaupasan142@users.noreply.github.com>
2021-06-26 18:31:42 +0530

    Added Project Files

---
+++ server/public/javascripts/index.js
@@ -0,0 +1,78 @@
+(function() {
+
+    $('.answers').hide();
+    $('.loading').hide();
+    $('#form').submit(onFormSubmit);
+    $('#age').val('40');
+    $('#sex').val('1');
+    $('#cp').val('0');
+    $('#trestbps').val('125');
+    $('#chol').val('220');
+    $('#fbs').val('0');
+    $('#restecg').val('1');
+    $('#thalach').val('144');
+    $('#exang').val('0');
+    $('#oldpeak').val('0.4');
+    $('#slope').val('1');
+    $('#ca').val('4');
+    $('#thal').val('3');
+    
+
+    function onFormSubmit() {
+        var age = $('#age').val();
+        var sex = $('#sex').val();
+        var cp = $('#cp').val();
+        var trestbps = $('#trestbps').val();
+        var chol = $('#chol').val();
+        var fbs = $('#fbs').val();
+        var restecg = $('#restecg').val();
+        var thalach = $('#thalach').val();
+        var exang = $('#exang').val();
+        var oldpeak = $('#oldpeak').val();
+        var slope = $('#slope').val();
+        var ca = $('#ca').val();
+        var thal = $('#thal').val();
+        $('.loading').show();
+        $('.answers').hide();
+        $('.classify-btn').prop('disabled', true);
+        $.post("/classify", {age: age,
+                             sex: sex,
+                             cp: cp,
+                             trestbps: trestbps,
+                             chol: chol,
+                             fbs: fbs,
+                             restecg: restecg,
+                             thalach: thalach,
+                             exang: exang,
+                             oldpeak: oldpeak,
+                             slope: slope,
+                             ca: ca,
+                             thal: thal
+                            }, function(data) {
+            renderAnswer(data)
+        }).fail(function(err) {
+            renderAnswer(err);
+        });
+        return false;
+    }
+
+    function renderAnswer(parsedResponse) {
+        console.log(parsedResponse);
+
+        if (parsedResponse.errors) {
+            $('.answer').html('Something went wrong :-( ' + parsedResponse.errors[0].message);
+        } else {
+            var data = parsedResponse.predictions[0].values[0]
+            var risk = data[0]
+            var prediction = data[1][0]
+            var probability = data[1][1]
+            $('.risk').html('Heart Risk: '+risk);
+            $('.prediction').html('Prediction: '+Math.floor(prediction*100 ).toFixed(0)+'%');
+            $('.probability').html('Probability: '+Math.floor(probability*100 ).toFixed(0)+'%');
+        }
+
+        $('.classify-btn').prop('disabled', false);
+        $('.answers').show();
+        $('.loading').hide();
+    }
+}());
